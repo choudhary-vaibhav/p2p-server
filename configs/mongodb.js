e0b4e47798d255ad649f1e8f937ad8aafbbea74e
Vaibhav Choudhary <vaibhavch29@gmail.com>
2023-12-01 21:30:00 +0530

    initial setup

---
+++ configs/mongodb.js
@@ -0,0 +1,11 @@
+// MONGO DB Configuration
+const mongoose = require("mongoose");
+
+const URL_VALUE = process.env.MONGODEV;
+
+//Connecting to DB.....
+mongoose
+  .connect(URL_VALUE, {
+  })
+  .then(() => console.log("Mongo DB Connected"))
+  .catch((err) => console.log(err.message));
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+require('./configs/mongodb');
+
+app.use(express.json());  //middleware to parse json
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors({origin: '*'}));
+
+// app.use('/',require('./routers/userRouters')); //dynamic routing
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server running at ' + port + '...'));
